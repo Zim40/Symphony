97e9687bfc031319a49f93ad52f1987ade0e63ec
michael monaghan <michaelm810129@gmail.com>
2024-10-22 18:58:40 +1030

    changed logo - working on navbar

--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -3,20 +3,17 @@ import image_1 from "../../assets/main(2).jpg";
 import Navbar from "./navigation/navbar.jsx";
 
 
-/* Only do standard image for background and blur it for individual sections. */
+/* sort image overlay extended beyond border */
 export default function Hero() {
 
 
     return (
         <>
             <Navbar />
-            <div className="relativeh-full">
+            <div className="Symph
+ny">
                 <img src={image_1} className="h-full w-full absolute top-0 left-0 object-cover z-0" />
-                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 flex-col min-h-full w-full">
-                    <h1 className="text-white text-5xl font-semibold">Find your Symphony.</h1>
-                    <button className="origin-bottom hover:rotate-6 duration-200 border-2 mt-6 px-4 py-2 rounded-xl w-1/5 bg-black bg-opacity-30 shadow-xl hover:border-black hover:shadow-white hover shadow-md">Start Here</button>
-
-                </div>
+                <div className=" absolute top-0 left-0 h-full w-full bg-black bg-opacity-20"></div>
             </div>
         </>
     )
--- src/Components/Hero/navigation/navbar.jsx
+++ src/Components/Hero/navigation/navbar.jsx
@@ -1,22 +1,65 @@
+import { useState } from "react";
+
 import brand from "../../../assets/Symphony.png";
+import { IoIosSearch } from "react-icons/io";
+import { RiMenu3Line } from "react-icons/ri";
 
 
 /* Set class to hidden for standard navbar then build a seperate mobile navbar for smailler devices.
 -- focus on creating navbars and then adding functionality reflecting a single page application (SPA). */
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
+
 
     return (
         <>
-            <div  className={`bg-black bg-opacity-20 relative w-full fixed z-40  backdrop-blur-sm`}>
-                <div>
-                    <ul className="flex space-x-3 items-center p-4">
-                        <img src={brand} alt="logo" className="text-sm mr-auto  w-auto h-[60px] lg:h-[50px]" />
-                        <li className="">Home</li>
-                        <li className="">Explore</li>
-                        <li className="">Contact</li>
+            <div className={` relative w-full fixed z-40  backdrop-blur-sm `}>
+                <div className="p-2">
+                    
+                    <ul className="flex items-center space-x-6 px-4">
+                        <img src={brand} alt="logo" className="text-sm mr-auto  w-auto h-[30px] lg:h-[px]" />
+                        <div className="flex items-center space-x-2 w-full justify-center">
+                            <label><IoIosSearch className="w-auto h-6" /></label>
+                            <input type="text" name="searchBar"
+                                placeholder="Search" className=" 
+                            border border-slate-600
+                            rounded backdrop-blur-lg bg-transparent w-full lg:w-1/2 px-2  placeholder:px-2 placeholder:text-slate-600"></input>
+                        </div>
+                        <li className="cursor-pointer">Home</li>
+                        <li className="cursor-pointer">Explore</li>
+                        <li className="cursor-pointer">Contact</li>
                     </ul>
-                </div>
+                    
+                    
+                </div> 
+               
+                {/* <MobileNav toggleMenu={toggleMenu} /> */}
             </div>
         </>
     )
-}
+};
+
+// const MobileNav = ({ toggleMenu }) => {
+//     return (
+//         <>
+//         <div className="block text-righ"><RiMenu3Line /></div>
+//             <div className="w-full bg-gray-400">
+//                 <div className="flex flex-col justify-end items-end">
+//                     <ul>
+//                         <li className="cursor-pointer">Home</li>
+//                         <li className="cursor-pointer">Explore</li>
+//                         <li className="cursor-pointer">Contact</li>
+//                     </ul>
+//                 </div>
+
+//             </div>
+
+//         </>
+
+//     )
+// }
+
