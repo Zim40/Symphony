bbf22ad129749b40e2b39d72145f69accede3878
michael monaghan <michaelm810129@gmail.com>
2024-11-07 17:51:45 +1030

    basic express server setup with client side proxy

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
-import express from "express";
+const express = require("express");
+const app = express ();
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
